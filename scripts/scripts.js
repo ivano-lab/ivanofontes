22fe1db4a2bc79ea99aa54dddcc8e81c09387fe5
Ívano Fontes <ivanofnts@gmail.com>
2025-01-10 18:21:51 -0300

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navMenu = document.getElementById('nav-menu');
+
+mobileMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    mobileMenu.classList.toggle('active'); 
+});
